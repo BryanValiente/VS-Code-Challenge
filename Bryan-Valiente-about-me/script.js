b67c78ade99d52c902d085bc60029276bf5a1c52
Nftdysentryanemonemitinity <107972861+Nftdysentryanemonemitinity@users.noreply.github.com>
2022-06-21 21:48:05 -0700

    Add files via upload

---
+++ Bryan-Valiente-about-me/script.js
@@ -0,0 +1,9 @@
+function smolImg(x) {
+    x.style.height = "200px";
+    x.style.width = "300px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "233px";
+    x.style.width = "350px";
+  }
